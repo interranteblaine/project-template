cbaaef286c0271cc818e67ea919c19231b3605ae
Blaine Interrante <interrante.blaine@gmail.com>
2023-04-19 09:45:58 -0400

    Add optional validation in useForm hook

--- src/common/hooks/useForm.tsx
+++ src/common/hooks/useForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 interface Validation<ValueType> {
   isValid: (value: ValueType) => boolean;
@@ -13,24 +13,12 @@ type Errors<FormValues> = Record<keyof FormValues, string>;
 
 const useForm = <FormValues,>(
   initialValues: FormValues,
-  validations: Validations<FormValues>
+  validations?: Validations<FormValues>
 ) => {
   const [values, setValues] = useState<FormValues>(initialValues);
   const [errors, setErrors] = useState<Errors<FormValues>>(
     {} as Errors<FormValues>
   );
-  const [submitting, setSubmitting] = useState(false);
-  const [isValid, setIsValid] = useState(false);
-
-  useEffect(() => {
-    if (!submitting) return;
-    validate();
-    const hasErrors = Object.keys(errors).some(
-      (k) => !!errors[k as keyof FormValues]
-    );
-    setIsValid(!hasErrors);
-    setSubmitting(false);
-  }, [submitting]);
 
   const handleChange = <Key extends keyof FormValues>(
     name: Key,
@@ -42,21 +30,25 @@ const useForm = <FormValues,>(
 
   const handleSubmit = (onSubmit: (values: FormValues) => void) => {
     return () => {
-      setSubmitting(true);
-      if (!isValid) return;
+      const hasErrors = validate()
+      if (hasErrors) return;
       onSubmit(values);
     };
   };
 
   const validate = () => {
+    if (!validations) return false
+    let hasErrors = false
     for (const key of Object.keys(validations)) {
       const value = values[key as keyof FormValues];
       const tests = validations[key as keyof FormValues];
       for (const test of tests) {
         if (test.isValid(value)) continue;
         setErrors((state) => ({ ...state, [key]: test.message }));
+        hasErrors = true
       }
     }
+    return hasErrors
   };
 
   const handleReset = () => {
