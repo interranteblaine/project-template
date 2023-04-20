5bba5e40e8df77662e17113551ce0659a620467f
Blaine Interrante <interrante.blaine@gmail.com>
2023-04-19 21:08:01 -0400

    Add visual modes

--- src/common/layout/MainTopNav.tsx
+++ src/common/layout/MainTopNav.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TopNavigation } from "@cloudscape-design/components";
+import { applyMode, applyDensity, Density, Mode } from '@cloudscape-design/global-styles';
+import '@cloudscape-design/global-styles/index.css'
 
 const MainTopNav = () => {
+    const [darkMode, setDarkMode] = useState(false)
+    const [compactMode, setCompactMode] = useState(false)
+
+    useEffect(() => {
+      const mode = darkMode ? Mode.Dark : Mode.Light
+      applyMode(mode)
+    }, [darkMode])
+
+    useEffect(() => {
+      const density = compactMode ? Density.Compact : Density.Comfortable
+      applyDensity(density)
+    }, [compactMode])
+
     return (
         <div id="h" style={{ position: "sticky", top: 0, zIndex: 1002 }}>
         <TopNavigation
@@ -11,6 +26,18 @@ const MainTopNav = () => {
           }}
           identity={{ href: "#", title: "App" }}
           utilities={[
+            {
+              type: 'button',
+              variant: 'link',
+              text: darkMode ? 'Light Mode' : 'Dark Mode',
+              onClick: () => setDarkMode(!darkMode)
+            },
+            {
+              type: 'button',
+              variant: 'link',
+              text: compactMode ? 'Comfortable Mode' : 'Compact Mode',
+              onClick: () => setCompactMode(!compactMode)
+            },
             {
               type: "menu-dropdown",
               text: "Customer Name",
